e1b4c92a4a9ae18f4180235f1c29fb78527708ea
CenIII <cenchuan2013@gmail.com>
2018-05-10 22:10:30 -0400

    ...

--- data/coco_pose/ref.py
+++ data/coco_pose/ref.py
@@ -8,6 +8,7 @@ from pycocotools import mask
 
 data_dir = '/home/chuancen/CVResearch/HumanPoseTracking/PJDATA/COCO/images'
 ann_path = '/home/chuancen/CVResearch/HumanPoseTracking/PJDATA/COCO/annotations/person_keypoints_train2014.json'
+
 ref_dir = os.path.dirname(__file__)
 
 assert os.path.exists(data_dir)
